20e85e07cb947345b1e31fbc11fc5b85a72fea02
hwangjaeyoon11 <hwangjaeyoon11@gmail.com>
2024-08-06 19:28:40 +0900

    init project

---
+++ src/test/java/com/naturecycle/naturecycle/NaturecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturecycle.naturecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
